5aeb53ed43ee82a430e2c551c6ff87fbba49c444
Chanpasong Thipphakhinkeo <Chanpasong.Thipphakhinkeo@gmail.com>
2026-03-15 08:02:16 -0400

    init upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CssBaseline from "@mui/material/CssBaseline";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <h1>Todos</h1>
+    </>
+  );
+}
+
+export default App;
